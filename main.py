6080059441e281d9c19c013b63d00bac5501697e
samrasiddiqui1 <samrasiddiquiofficial@gmail.com>
2025-04-21 14:23:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    anton:int = 21
+    Beth:int = 21 + 6
+    Chen:int = 20 + Beth
+    drew: int = Chen + anton
+    Ethen: int = Chen
+
+
+    print(f'Anton: {anton}')
+    print(f'Beth: {Beth}')
+    print(f'Chen: {Chen}')
+    print(f'drew: {drew}')
+    print(f'Ethan: {Ethen}')
+
+
+
+main()
